4b41931dc4bca8e0e5a50cde3e8fdffd9c88abbf
yangyuexiong <yang6333yyx@126.com>
2019-11-06 17:34:50 +0800

    再也不想用jinjia2了

--- app/controllers/show_reports/module_1.py
+++ app/controllers/show_reports/module_1.py
@@ -5,6 +5,7 @@
 # @File    : module_1.py
 # @Software: PyCharm
 
+import os
 
 from flask import Blueprint, render_template
 from config.config import app_conf, config_obj
@@ -13,17 +14,48 @@ route_module_01 = Blueprint('module_01', __name__)
 
 R = config_obj[app_conf()].R
 
-exchange = 'bitfinex'
-exchange_key = 'exchange:%s' % exchange
-R.hgetall(exchange_key).items()
-
 
 @route_module_01.route('/index', methods=["GET", "POST"])
-def index():
-    context = R.hgetall(exchange_key).items()
-    return render_template('index.html', context=context)
+@route_module_01.route('/index/<ex>', methods=["GET", "POST"])
+def index(ex=None):
+    """
 
+    :param ex:
+    :return:
+    """
+    path_1 = os.getcwd().split('AirCurrencyWeb')[0]  # 系统目录
+    path_2 = 'AirCurrencyWeb/app/static'  # 项目目录
+    report_dir = path_1 + path_2
+    lists = os.listdir(report_dir)  # 报告列表
+    lists.sort(key=lambda fn: os.path.getmtime(report_dir + '/' + fn))  # 排序
 
-@route_module_01.route('/rp_list', methods=["GET", "POST"])
-def report_list():
-    return render_template('测试报告_2019-11-05 15_57_14_.html')
+    if ex == 'okex':
+        print('okex')
+        exchange = 'okex'
+        exchange_key = 'exchange:%s' % exchange
+        context = R.hgetall(exchange_key).items()
+        return render_template('index.html', context=context, r_list=lists, ex=ex)
+
+    if ex == 'bitfinex':
+        print('bitfinex')
+        exchange = 'bitfinex'
+        exchange_key = 'exchange:%s' % exchange
+        context = R.hgetall(exchange_key).items()
+        return render_template('index.html', context=context, r_list=lists, ex=ex)
+
+    if ex == 'huobi':
+        print('huobi')
+        exchange = 'huobi'
+        exchange_key = 'exchange:%s' % exchange
+        context = R.hgetall(exchange_key).items()
+        return render_template('index.html', context=context, r_list=lists, ex=ex)
+
+    else:
+        print('all')
+        R.keys('exchange*')
+        print(R.keys('exchange*'))
+        all_err_exchang = {}
+        for i in R.keys('exchange*'):
+            all_err_exchang.update(R.hgetall(i))
+        context = all_err_exchang.items()
+        return render_template('index.html', context=context, r_list=lists)
